193fd07d87f59eb954d2cc849635e81e30c1ba8d
pa4080 <spas.z.spasov@gmail.com>
2017-10-11 13:37:34 +0300

    Just update

---
+++ install.bash
@@ -0,0 +1,43 @@
+#!/bin/bash
+HEIGHT=11
+CHOICE_HEIGHT=3
+WIDTH=90
+BACKTITLE="Simple installer"
+TITLE="[ Traffic Watch Installer ]"
+MENU="Create symbolic link or copy the script:"
+
+OPTIONS=(
+SymLink   ' sudo ln -s $(pwd)/traffic-watch.bash /usr/local/bin/traffic-watch '
+Copy      ' sudo cp $(pwd)/traffic-watch.bash /usr/local/bin/traffic-watch    '
+Shell     " Back to CLI"
+)
+
+CHOICE=$(whiptail --clear \
+    --backtitle "$BACKTITLE" \
+    --title "$TITLE" \
+    --menu "$MENU" \
+    $HEIGHT $WIDTH $CHOICE_HEIGHT \
+    "${OPTIONS[@]}" \
+    2>&1 >/dev/tty)
+
+clear
+case $CHOICE in
+    SymLink)
+        sudo chmod +x "$(pwd)/traffic-watch.bash"
+        sudo ln -s "$(pwd)/traffic-watch.bash" "/usr/local/bin/traffic-watch"
+        ;;
+
+    Copy)
+        sudo chmod +x "$(pwd)/traffic-watch.bash"
+        sudo cp "$(pwd)/traffic-watch.bash" "/usr/local/bin/traffic-watch"
+        ;;
+
+
+    Shell)
+        exit
+        ;;
+esac
+
+
+
+
