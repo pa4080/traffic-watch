948a094faaa65df3425294b5f0d543ce32c564f0
pa4080 <spas.z.spasov@gmail.com>
2017-10-11 12:50:38 +0300

    The stable version
    `/sbin` is not included in Cron `$PATH` (=`/bin:/usr/bin`).

--- traffic-watch.bash
+++ traffic-watch.bash
@@ -7,36 +7,32 @@ LOG="/tmp/traffic-watch-$IFACE.log"         # Set the log file name
 LANG=C                                      # Set envvar $LANG to `C` due to grep, awk, etc.
 IPPT='[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+'       # Set IP address match pattern #IPPT='[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}'
 
-NIC="$(ethtool -i "$IFACE" | grep "driver:" | sed 's/driver:\ //')"              # Get the $IFACE (NIC) driver
+NIC="$(/sbin/ethtool -i "$IFACE" | awk 'FS=": " {print $2; exit}')" # Get the $IFACE (NIC) driver
 
 # Function: Get the current traffic
 get_traffic(){
-    RX="$(ifconfig "$IFACE" | grep -Po "RX bytes:[0-9]+" | sed 's/RX bytes://')" # Get the incoming traffic
-    TX="$(ifconfig "$IFACE" | grep -Po "TX bytes:[0-9]+" | sed 's/TX bytes://')" # Get the outgoing traffic
+    RX="$(/sbin/ifconfig "$IFACE" | grep -Po "RX bytes:[0-9]+" | sed 's/RX bytes://')" # Get the incoming traffic
+    TX="$(/sbin/ifconfig "$IFACE" | grep -Po "TX bytes:[0-9]+" | sed 's/TX bytes://')" # Get the outgoing traffic
     XB=$(( RX + TX ))                                                            # Calculate the total traffic
     XM=$(( XB / ( 1000 * 1000 ) ))                                               # Convert the total traffic in MB
 }
 
 # Functions: Disable the interface
-interface_down(){
-    ifconfig "$IFACE" down 2>/dev/null && exit
-}
+interface_down(){ /sbin/ifconfig "$IFACE" down 2>/dev/null && exit; }
 
 # Function: Reset the traffic and enable the interface
-reset_traffic_interface_up(){ modprobe -r "$NIC" 2>/dev/null && modprobe "$NIC" 2>/dev/null && ifconfig "$IFACE" up 2>/dev/null; }
+reset_traffic_interface_up(){ /sbin/modprobe -r "$NIC" 2>/dev/null && /sbin/modprobe "$NIC" 2>/dev/null && /sbin/ifconfig "$IFACE" up 2>/dev/null; }
 
 # Function: Get the IP address
-get_ip(){ ifconfig "$IFACE" 2>/dev/null | grep -Po "${IPPT}" | head -1; }
+get_ip(){ /sbin/ifconfig "$IFACE" 2>/dev/null | grep -Po "${IPPT}" | head -1; }
 
 # --- The main program ---
 
 reset_traffic_interface_up
 
-# Wait until an IP address is obtained
-until [[ "$(get_ip)" =~ ${IPPT} ]]; do
-    sleep 1
-done
-echo "${IPPT}"
+# Wait until the IP address is obtained
+until [[ "$(get_ip)" =~ ${IPPT} ]]; do sleep 1; done
+
 # While the interface has IP address == while it is up; check if it is up on every 5 seconds (the `time` of the cycle is about 75 ms)
 while [[ "$(get_ip)" =~ ${IPPT} ]]; do
 
@@ -54,9 +50,8 @@ while [[ "$(get_ip)" =~ ${IPPT} ]]; do
             printf '\n%s MB remains on %s.\n\n' "$(( LIMIT - XM ))" "$IFACE" >> "$LOG"
     fi
 
-    # Debug:
-    cat "$LOG"
+    # Debug:    cat "$LOG"
 
-    sleep 5 # Adjust this value
+    sleep 5 ## *Adjust this value* ##
 
-done
+done; interface_down
